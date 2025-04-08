1a070fbe87fa1a9f1b5e7c32a2c379a8f96a6bb1
adenijimuiz <muizodunayo111@gmail.com>
2025-04-08 09:55:02 +0100

    added components

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function SideBar() {
+  return (
+    <div className='sidebar'>
+      <div className="bgOverlay">
+        <div className='sidebarContents'>
+          <h2>The Brutal Martian LandScape</h2>
+          <p>Description</p>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat ab qui veniam tempore reprehenderit dolor illum at ex eaque expedita libero ea quibusdam perspiciatis, aspernatur recusandae. Deserunt autem rerum saepe qui incidunt praesentium, quibusdam culpa.</p>
+        </div>
+      </div>
+    </div>
+  )
+}
