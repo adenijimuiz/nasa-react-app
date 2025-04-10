1c2ec60319ef0a7892ae4e6561b7a06fba89b903
adenijimuiz <muizodunayo111@gmail.com>
2025-04-10 21:17:34 +0100

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -13,11 +13,22 @@ function App() {
     async function fetchApiData() {
       const NASA_KEY = import.meta.env.VITE_NASA_API_KEY;
       const url = 'https://api.nasa.gov/planetary/apod' + `?api_key=${NASA_KEY}`;
+
+      const today = (new Date()).toDateString();
+      const localKey = `NASA-${today}`
+      if (localStorage.getItem(localKey)) {
+        const apiData = JSON.parse(localStorage.getItem(localKey));
+        setData(apiData);
+        return;
+      }
+      localStorage.clear()
+
       try {
         const res = await fetch(url);
         const apiData = await res.json();
-        setData(apiData)
-        console.log('DATA\n', apiData)
+        localStorage.setItem(localKey, JSON.stringify(apiData))
+        setData(apiData);
+        // console.log('DATA\n', apiData)
       } catch (error) {
         console.log(error.message);
       }
