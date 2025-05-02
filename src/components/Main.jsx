72359c2bb92033b84638787e8c624fdb4cc8527b
adenijimuiz <muizodunayo111@gmail.com>
2025-05-02 21:26:37 +0100

    updated

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,8 +4,9 @@ export default function Main(props) {
   const {data} = props;
   return (
     <div className="imgContainer">
-      <img src={data?.hdurl} alt={data?.title || 'bg-img'} className='bgImage'/>
-      {/* <img src={pic} alt="" /> */}
+      {/* <div className="bg" style={{backgroundImage: `url(${data?.hdurl})`}}>
+      </div> */}
+      <img src={data?.hdurl} alt={data?.title || 'bg-img'} className='bgImage' style={{backgroundImage: data?.hdurl}}/>
     </div>
   )
 }
